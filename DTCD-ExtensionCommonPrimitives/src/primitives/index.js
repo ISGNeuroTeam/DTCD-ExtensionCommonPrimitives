b96b173b9b88496498425a56599313f577e1318a
romankuramshin <rkuramshin@isgneuro.com>
2021-11-10 22:51:05 +0300

    added generators for nodes with prots and icons for them

--- DTCD-ExtensionCommonPrimitives/src/primitives/index.js
+++ DTCD-ExtensionCommonPrimitives/src/primitives/index.js
@@ -1,27 +1,23 @@
 // import SimpleNode from './SimpleNode';
-// import SimpleEdge from './SimpleEdge';
+import SimpleEdge from './SimpleEdge';
 // import DashedEdge from './DashedEdge';
 // import TargetNode from './TargetNode';
 // import RiskNode from './RiskNode';
 // import TransformationNode from './TransformationNode';
-// import MathOperationNode from './MathOperationNode';
+import MathOperationNode from './MathOperationNode';
 // import InPort from './InPort';
 // import OutPort from './OutPort';
-import TargetRichLabelNode from './RichLabelNodes/TargetRichLabelNode'
-import StepRichLabelNode from './RichLabelNodes/StepRichLabelNode'
-import ControlledRichLabelNode from './RichLabelNodes/ControlledRichLabelNode'
-import UncontrolledRichLabelNode from './RichLabelNodes/UncontrolledRichLabelNode'
+import CRLNodes from './RichLabelNodes/CRLNodes'
+import URLNodes from './RichLabelNodes/URLNodes'
+import SRLNodes from './RichLabelNodes/SRLNodes'
+import TRLNodes from './RichLabelNodes/TRLNodes'
+
 
 export default [
-  // InPort,
-  // OutPort,
-  // SimpleEdge,
-  // TargetNode,
-  // MathOperationNode,
-  // TransformationNode,
-  // RiskNode,
-  TargetRichLabelNode,
-  StepRichLabelNode,
-  ControlledRichLabelNode,
-  UncontrolledRichLabelNode
+  SimpleEdge,
+  MathOperationNode,
+  ...CRLNodes,
+  ...URLNodes,
+  ...SRLNodes,
+  ...TRLNodes
 ];
