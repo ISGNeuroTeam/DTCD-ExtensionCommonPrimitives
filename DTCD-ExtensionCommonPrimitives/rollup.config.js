80f293ed0e27056f900f5221486d70fb82423c9a
sbelikov98 <sbelikov@isgneuro.com>
2022-02-14 13:20:36 +0300

    Changed build process

--- DTCD-ExtensionCommonPrimitives/rollup.config.js
+++ DTCD-ExtensionCommonPrimitives/rollup.config.js
@@ -1,16 +1,18 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import img from '@rollup/plugin-image';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { version } from './package.json';
 
 const watch = Boolean(process.env.ROLLUP_WATCH);
 
 const pluginName = 'ExtensionCommonPrimitives';
 
 const output = watch
-  ? `./../../DTCD/server/plugins/DTCD-${pluginName}/${pluginName}.js`
+  ? `./../../DTCD/server/plugins/DTCD-${pluginName}_${version}/${pluginName}.js`
   : `./build/${pluginName}.js`;
 
-const plugins = [commonjs(), nodeResolve(), img()];
+const plugins = [json(), commonjs(), nodeResolve(), img()];
 
 export default {
   input: `./src/${pluginName}.js`,
