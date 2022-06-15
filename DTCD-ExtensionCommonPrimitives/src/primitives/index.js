a8fd676a332fbf15fc881bcb8376210fc98f0af8
rkuramshin <rkuramshin@isgneuro.com>
2022-06-15 11:43:58 +0400

    removed fgk primitives to another extension

--- DTCD-ExtensionCommonPrimitives/src/primitives/index.js
+++ DTCD-ExtensionCommonPrimitives/src/primitives/index.js
@@ -3,21 +3,9 @@ import SimpleEdge from './SimpleEdge';
 // import DashedEdge from './DashedEdge';
 // import TargetNode from './TargetNode';
 // import RiskNode from './RiskNode';
-// import TransformationNode from './TransformationNode';
-import MathOperationNode from './MathOperationNode';
+// import GroupNode from './GroupNode';
+// import MathOperationNode from './MathOperationNode';
 // import InPort from './InPort';
 // import OutPort from './OutPort';
-import CRLNodes from './RichLabelNodes/CRLNodes'
-import URLNodes from './RichLabelNodes/URLNodes'
-import SRLNodes from './RichLabelNodes/SRLNodes'
-import TRLNodes from './RichLabelNodes/TRLNodes'
 
-
-export default [
-  SimpleEdge,
-  MathOperationNode,
-  ...CRLNodes,
-  ...URLNodes,
-  ...SRLNodes,
-  ...TRLNodes
-];
+export default [SimpleEdge];
